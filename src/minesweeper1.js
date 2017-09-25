a368572c207e67f2656336d453d5e9561b825719
bentleybuilds <bentleybuilds@gmail.com>
2017-09-25 12:46:14 -0600

    initializing minesweeper.js project git repo

---
+++ src/minesweeper1.js
@@ -0,0 +1,13 @@
+const blankLine = ' | | ';
+const guessLine = '1| | ';
+const bombLine = ' |B| ';
+console.log(`This is what a blank board looks like`);
+// Hard coded blank 3x3 board
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+console.log(`This is what a board with a guess and a bomb on it would look like`);
+// This is a static board with a bomb and a guess
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
